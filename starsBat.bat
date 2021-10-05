b13606066921c185c8d2bbb6e734a23581724015
riki-wi <riki-wi@yandex.ru>
2021-10-05 22:12:19 +0300

    Добавлен вывод звездочек справа и по центру

--- starsBat.bat
+++ starsBat.bat
@@ -29,3 +29,41 @@ for /L %%i in (1,1,%1) do (
 echo.
 echo ===============
 echo.
+
+set string=
+
+for /L %%i in (1,1,%1) do (
+	set /a n=%1-%%i-1
+	for /L %%j in (!n!,-1,0) do set string=!string! 
+	for /L %%j in (1,1,%%i) do set string=!string!*
+	echo !string!
+	set string=
+)
+		
+
+echo.
+echo ===============
+echo.
+
+set string=
+
+for /L %%i in (1,1,%1) do (
+	set /a n=%1-%%i
+	for /L %%j in (!n!,-1,1) do set string=!string! 
+	set /a n=2*%%i-2
+	for /L %%j in (0,1,!n!) do set string=!string!*
+	echo !string!
+	set string=
+)
+
+set /a x=%1-1
+for /L %%i in (!x!,-1,1) do (
+	set /a n=%1-%%i
+	for /L %%j in (!n!,-1,1) do set string=!string! 
+	set /a n=2*%%i-2
+	for /L %%j in (0,1,!n!) do set string=!string!*
+	echo !string!
+	set string=
+)	
+
+
